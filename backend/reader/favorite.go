a5bc7515e42c151f1d30e32bfd2d7d7fcff2e548
HowJMay <yuanyanghau@gmail.com>
2022-07-11 21:50:03 +0700

    feat: Implement reader.GetFavoriteList

--- backend/reader/favorite.go
+++ backend/reader/favorite.go
@@ -1,8 +1,27 @@
 package reader
 
-import "github.com/manga-reader/manga-reader/backend/database"
+import (
+	"encoding/json"
+	"fmt"
 
-func (r *Reader) GetFavoriteList() ([]database.ComicInfo, error) {
+	"github.com/manga-reader/manga-reader/backend/database"
+)
 
-	return nil, nil
+func (r *Reader) GetFavoriteList(db *database.Database) ([]database.ComicInfo, error) {
+	resRaws, err := db.ListRangeAll(database.GetUserFavoriteKey(r.ID))
+	if err != nil {
+		return nil, fmt.Errorf("can't get user %s's favorite list: %w", r.ID, err)
+	}
+
+	res := make([]database.ComicInfo, len(resRaws))
+	for i, raw := range resRaws {
+		var tmp database.ComicInfo
+		err = json.Unmarshal([]byte(raw), &tmp)
+		if err != nil {
+			return nil, fmt.Errorf("can't unmarshal raw message  '%s' : %w", raw, err)
+		}
+		res[i] = tmp
+	}
+
+	return res, nil
 }
